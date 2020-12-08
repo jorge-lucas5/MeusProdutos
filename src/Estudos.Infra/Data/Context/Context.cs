bc16c70152dbeacae0fc89cc72494e637637ce92
jorgelucas <jorgelucasl91@gmail.com>
2020-12-08 16:16:26 -0300

    ciração da classe de contexto

---
+++ src/Estudos.Infra/Data/Context/Context.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Estudos.business.Models.Fornecedores;
+using Estudos.business.Models.Produtos;
+
+namespace Estudos.Infra.Data.Context
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DbConnection")
+        { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+        public DbSet<Fornecedor> Fornecedor { get; set; }
+    }
+}
