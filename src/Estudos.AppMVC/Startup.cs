6bf93bfc7d4d8a7e86a6955e7face9edf68dcf1b
jorgelucas <jorgelucasl91@gmail.com>
2020-12-15 15:53:57 -0300

    centralizando as configurações

--- src/Estudos.AppMVC/Startup.cs
+++ src/Estudos.AppMVC/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Estudos.AppMVC.Startup))]
@@ -9,6 +12,12 @@ namespace Estudos.AppMVC
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            DependencyInjectionConfig.RegisterDIContainer();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
